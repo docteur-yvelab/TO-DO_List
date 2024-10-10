9dfeeb5574d2309073b4c3f01a2b9d37c64799cc
yve <docteuryve9@gmail.com>
2024-10-10 09:39:01 +0100

    Update TO-DO_list.js

--- TO-DO_list.js
+++ TO-DO_list.js
@@ -26,26 +26,3 @@ btn2.addEventListener("click", () => {
     console.log("supprimer");
     task1.value = "";
 });
-
-
-// convertiseur en binaire !!!
-
-// function textToBinary(text) {
-//     let binaryResult = '';
-
-//     for (let i = 0; i < text.length; i++) {
-//         // Obtenir le code ASCII de chaque caractère
-//         const charCode = text.charCodeAt(i);
-//         // Convertir le code ASCII en binaire et le formater sur 8 bits
-//         const binaryChar = charCode.toString(2).padStart(8, '0');
-//         // Ajouter le binaire à la chaîne de résultat
-//         binaryResult += binaryChar + ' '; // Ajoute un espace entre les caractères
-//     }
-
-//     return binaryResult.trim(); // Supprime l'espace final
-// }
-
-// // Exemple d'utilisation
-// const inputText = "134D";
-// const binaryOutput = textToBinary(inputText);
-// console.log(binaryOutput); // Affiche "01000001 01000010 01000011"
